ae21ad4bded2b6060baf9b5803eb51efb1aef68e
yunsteel <archer0082@naver.com>
2024-02-02 19:26:00 +0900

    투두리스트

--- App.tsx
+++ App.tsx
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [list, setList] = useState<string[]>([]);
+
+  const handleChangeInput = (inputText: string) => {
+    setInputValue(inputText);
+  };
+
+  const handlePressButton = () => {
+    setList((list) => [...list, inputValue]);
+    setInputValue("");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="입력해라"
+          onChangeText={handleChangeInput}
+          value={inputValue}
+        />
+        <Button title="클릭해라" onPress={handlePressButton} />
+      </View>
+      <View style={styles.goalsContainer}>
+        {list.map((item, index) => (
+          <Text key={item + index}>{item}</Text>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingVertical: 50,
+    paddingHorizontal: 30,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: 24,
+    borderBottomWidth: 10,
+    borderBottomColor: "lightgray",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "black",
+    width: "80%",
+    marginRight: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+  },
+  goalsContainer: {
+    flex: 4,
+    backgroundColor: "pink",
   },
 });
